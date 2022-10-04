5e549da542e3862016809fcd7489e8d417da4c8b
tuantai96utc <tuantai96utc@gmail.com>
2022-10-04 23:38:01 +0700

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+   <div>
+      <button class="btn btn-success" @click="increment">Increment</button>&ensp;
+      <button class="btn btn-success" @click="decrement">Decrement</button>
+   </div>
+</template>
+<script>
+import {mapMutations} from 'vuex'
+
+export default {
+   data() {
+      return {};
+   },
+   methods: {
+      increment() {
+         this.$store.commit("incrementOp", 30);
+      },
+      ...mapMutations([
+         "decrement"
+      ])
+   },
+
+}
+</script>
---
+++ src/components/Result.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+      <p>This is results: {{ result }}</p>
+   </div>
+</template>
+<script>
+
+export default {
+   computed: {
+      result() {
+         return this.$store.getters.result;
+      }
+   }
+}
+</script>
